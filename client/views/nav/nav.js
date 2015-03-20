8ddcaa893c3a92a5b7810a9e537d78e51b5cc63f
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-20 16:22:40 -0300

    Add logo

--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -18,7 +18,7 @@ Template[getTemplate('nav')].helpers({
     return getTemplate(this.template);
   },
   site_title: function(){
-    return getSetting('title', "Telescope");
+    return getSetting('title', "Conectação");
   },
   logo_url: function(){
     return getSetting('logoUrl');
